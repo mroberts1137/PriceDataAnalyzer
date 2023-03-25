6f2fd151b6592dd0c59a60a17a9254e0d65a4043
mroberts1137 <mroberts1137@gmail.com>
2023-03-25 18:34:18 -0400

    Fixed venv dependencies. Added requirements.txt

--- load_dataset/DataProcessing.py
+++ load_dataset/DataProcessing.py
@@ -10,79 +10,78 @@ import csv
 import pickle
 
 
-#lambdas = np.array([3, 5, 7, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 140, 160, 180, 200])
-#lambdas = np.arange(1, 201)
-#warmup_period = lambdas.max()
-#polynomial_order = 8
-#x_max_length = 20
-
-#x_warmup = pd.read_csv('SPY-daily-2021-2023.csv', header=0, usecols=['Date', 'Close'], nrows=warmup_period)
-#x_data = pd.read_csv('SPY-daily-2021-2023.csv', header=0, usecols=['Date', 'Close'], skiprows=range(1,warmup_period+1))
-
-
 class DataTransform:
-    def __init__(self, x_data, lambdas, polynomial_order, x_max_length):
+    def __init__(self, x_data, lambdas, polynomial_order, x_max_length, warmup=True):
 
-        self.rawdata_dir = os.path.join(os.path.join(os.getcwd(), 'load_dataset'), 'Raw_Data')
+        self.rawdata_dir = os.path.join(os.path.join(os.getcwd(), 'load_dataset'), 'raw_data')
         self.data_file = os.path.join(self.rawdata_dir, x_data)
 
+        self.warmup = warmup
         self.lambdas = lambdas
         self.warmup_period = self.lambdas.max()
+        self.x_max_length = x_max_length
+        self.polynomial_order = polynomial_order
 
-        self.x_warmup = pd.read_csv(self.data_file, header=0, usecols=['Date', 'Close'], nrows=self.warmup_period)
-        self.x = pd.read_csv(self.data_file, header=0, usecols=['Date', 'Close'], skiprows=range(1, self.warmup_period+1))
+        if self.warmup:
+            self.x_warmup = pd.read_csv(self.data_file, header=0, usecols=['Date', 'Close'], nrows=self.warmup_period)
+            self.x = pd.read_csv(self.data_file, header=0, usecols=['Date', 'Close'], skiprows=range(1, self.warmup_period+1))
+        else:
+            self.x = pd.read_csv(self.data_file, header=0, usecols=['Date', 'Close'])
 
-        self.mu = np.zeros(len(lambdas))
-        self.beta = np.zeros(polynomial_order + 1)
-        self.sigma = np.zeros(len(lambdas) + 1)
-        self.mu_slope = np.zeros(len(lambdas))
+        self.mu = np.zeros(len(self.lambdas))
+        self.beta = np.zeros(self.polynomial_order + 1)
+        self.sigma = np.zeros(len(self.lambdas) + 1)
+        self.mu_slope = np.zeros(len(self.lambdas))
         self.mu_list = []
         # self.beta_df = []
         for i in range(1, len(self.x.index)):
             self.x.loc[i, 'Diff'] = self.x.loc[i, 'Close'] - self.x.loc[i - 1, 'Close']
+        #self.price_change_mean = self.x['Diff'].mean()
+        #self.volatility = self.x['Diff'].std()
 
     def updateData(self, data_new):
         self.x = self.x.append(data_new, ignore_index=True)
-        if len(self.x.index) > x_max_length:
+        self.x.iloc[-1]['Diff'] = self.x.iloc[-1]['Close'] - self.x.iloc[-2]['Close']
+        if len(self.x.index) > self.x_max_length:
             self.x.drop(index=0, inplace=True)
             self.x.reset_index(drop=True, inplace=True)
-        for i in range(1, len(self.x.index)):
-            self.x.loc[i, 'Diff'] = self.x.loc[i, 'Close'] - self.x.loc[i - 1, 'Close']
-
         return self.x
 
-    def warmupSMA(self, x_data):
-        # x_data = np.pad(x_data, (lambdas.max(), 0), 'edge')
-        t0 = len(x_data)
-
-        for i, l in enumerate(lambdas):
-            data = x_data[t0 - l: t0]
-            self.mu[i] = np.mean(data)
-        return self.mu
+    def warmupSMA(self):
+        if self.warmup:
+            t0 = len(self.x_warmup)
+            for i, l in enumerate(self.lambdas):
+                self.mu[i] = np.mean(self.x_warmup[t0 - l: t0])
+            return self.mu
+        else:
+            print("Do not run warmupSMA unless warmup=True")
 
     def updateMu(self, x_new):
         # run BEFORE updataData since this requires oldest data value, x(t0 - lambda.max)
         # delta_mu ~ dmu/dt = (x(t) - x(t-lambda)) / lambda
 
-        for i, l in enumerate(lambdas):
-            t0 = len(self.x.index)
+        t0 = len(self.x.index)
+        for i, l in enumerate(self.lambdas):
             x_old = self.x.loc[t0 - l, 'Close']
             delta_mu = (x_new - x_old) / l
             self.mu[i] = self.mu[i] + delta_mu
             self.mu_slope[i] = delta_mu
-
         return self.mu
 
     def updateBeta(self):
-        self.beta = np.polyfit(lambdas, self.mu, polynomial_order)
+        self.beta = np.polyfit(self.lambdas, self.mu, self.polynomial_order)
         return self.beta
 
     def updateSigma(self):
-        for i in range(2, len(lambdas) + 1):
+        for i in range(2, len(self.lambdas) + 1):
             # take the standard deviation of the first i EMAs
             self.sigma[i] = np.std(self.mu[:i])
-
         return self.sigma
+
+    def initiate(self):
+        if self.warmup: self.warmupSMA()
+        self.updateBeta()
+        self.updateSigma()
 
     def step(self, data_new):
         self.updateMu(data_new['Close'])  # update mu before data since mu requires all old data
@@ -91,25 +90,28 @@ class DataTransform:
         self.updateSigma()
 
     def append_mu(self, date):
-        new_mu = {l: self.mu[i] for i, l in enumerate(lambdas)}
+        new_mu = {l: self.mu[i] for i, l in enumerate(self.lambdas)}
         new_mu['Date'] = date
         self.mu_list.append(new_mu)
 
-    def batchMu(self, data, warmup_data):
-        full_data = np.concatenate((warmup_data, data))
-        t0 = len(warmup_data)
-        mu = np.zeros((len(data), len(lambdas)))
+    def batchMu(self):
+        if self.warmup:
+            full_data = np.concatenate((self.x_warmup, self.x))
+            t0 = len(self.x_warmup.index)
+            mu = np.zeros((len(self.x.index), len(self.lambdas)))
 
-        for i, l in enumerate(lambdas):
-            for j in range(len(data)):
-                window = full_data[t0 + j + 1 - l: t0 + j + 1]
-                mu[j, i] = np.mean(window)
-        return mu
+            for i, l in enumerate(self.lambdas):
+                for j in range(len(self.x.index)):
+                    window = full_data[t0 + j + 1 - l: t0 + j + 1]
+                    mu[j, i] = np.mean(window)
+            return mu
+        else:
+            print("Do not run batchMu unless warmup=True")
 
     def batchBeta(self, mu):
-        betas = np.zeros((mu.shape[0], polynomial_order + 1))
+        betas = np.zeros((mu.shape[0], self.polynomial_order + 1))
         for i in range(mu.shape[0]):
-            betas[i] = np.polyfit(lambdas, mu[i], polynomial_order)
+            betas[i] = np.polyfit(self.lambdas, mu[i], self.polynomial_order)
         return betas
 
     def batchSigma(self, mu):
@@ -118,7 +120,7 @@ class DataTransform:
     def batchSlope(self):
         return
 
-    def x_from_mu(mu, lambdas):
+    def x_from_mu(self, mu, lambdas):
         # Gives the average value of x between all neighboring values of mu_lambda
         # Time-reversed array is easier to calculate with the ordering of lambda and mu_lambda
         # So we flip the output array
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-# This is a sample Python script.
+import os
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import csv
+import pickle
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from load_dataset import DataProcessing
+
+# Hyperparameters:
+
+lambdas = np.array([3, 5, 7, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 140, 160, 180, 200])
+#lambdas = np.arange(1, 201)
+polynomial_order = 8
+x_max_length = 20
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    dt = DataProcessing.DataTransform('SPY-daily-2021-2023.csv', lambdas, polynomial_order, x_max_length)
 
 
